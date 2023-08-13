4d769a0de71d0a995b6b53bddd03a9512a0e0098
Rockyers <ethan@yocom.org>
2023-08-13 01:50:35 -0700

    Inital Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PointsPlus
+A simple, easy to use plugin to keep track of everyone's score
---
+++ src/main/java/me/rockyers/pointsplus/PointsPlus.java
@@ -0,0 +1,17 @@
+package me.rockyers.pointsplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PointsPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
